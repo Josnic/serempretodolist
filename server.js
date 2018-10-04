204c33bb3e99113e99a5e0f25f679b80e1ad3176
Nicolás Herrera. SCO <jherrera@santillana.com>
2018-10-04 14:26:30 -0500

    Creación de un servidor http con express, habilitación de cors

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 //archivo main
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(function(req, res) {
+    response(res, 404, { message: "La url solicitada no se encuentra." });
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+});
