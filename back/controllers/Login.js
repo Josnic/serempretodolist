174d12844059003813217c9b73851f5f6c34f00b
Nicolás Herrera. SCO <jherrera@santillana.com>
2018-10-04 16:49:11 -0500

    Controlador que busca el usuario recibido via post en MongoDB

---
+++ back/controllers/Login.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+var User = require("../models/User");
+var sha1 = require("sha1");
+
+var Login = {};
+
+Login.validateUser = function(req, res) {
+    var objS = {
+        name: req.body.user,
+        password: sha1(req.body.pass)
+    }
+
+    User.find(objS, function(err, user) {
+        if (err) {
+            res.redirect("/errorLogin");
+        } else {
+            //code
+            if (user.length == 0) {
+                res.redirect("/errorLogin");
+            } else {
+                req.session.user = req.body.user;
+                req.session.admin = user[0].admin;
+                res.redirect("/home");
+            }
+        }
+    });
+
+}
