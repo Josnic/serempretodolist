b5ef81ad0fd97e9412f53706cf4a4fcac7b9192e
Nicolás Herrera. SCO <jherrera@santillana.com>
2018-10-05 15:07:32 -0500

    Integración exepress-session y socket.io

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ var app = express();
 var server = require('http').createServer(app);
 var path = require("path");
 var io = require('socket.io')(server);
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+sharedsession = require("express-socket.io-session");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+session = require("express-session")({
+        secret: "2C44-4D44-WppQ38S",
+        resave: true,
+        saveUninitialized: true
+    }),
+    sharedsession = require("express-socket.io-session");
+
+
+// Attach session
+app.use(session);
+
+// Share session with io sockets
+/*
+io.use(sharedsession(session));
 
 session = require('express-session');
 app.use(session({
@@ -16,6 +32,10 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+*/
+io.sockets.use(sharedsession(session));
+
+
 
 var mongoose = require('mongoose');
 var config = require('./back/config/config');
@@ -32,7 +52,8 @@ mongoose.connect(config.database, options); // connect to database
 
 // Authentication and Authorization Middleware
 var auth = function(req, res, next) {
-    if (req.session && req.session.admin) {
+    console.log(req.session)
+    if (req.session.user && req.session.admin !== null && req.session.user !== "undefined" && req.session.admin !== "undefined" && req.session.user !== "") {
         return next();
     } else {
         return res.sendStatus(401);
@@ -63,14 +84,14 @@ app.post("/login", function(req, res) {
 })
 
 
-app.get("/logout", function(req, res) {
-    req.session.destroy();
-    res.redirect("/index");
+app.get("/home", auth, function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
 })
 
 
-app.get("/home", auth, function(req, res) {
-    res.sendFile(path.join(__dirname, "/views/home.html"));
+app.get("/logout", function(req, res) {
+    req.session.destroy();
+    res.redirect("/index");
 })
 
 
@@ -87,6 +108,24 @@ app.use(function(req, res, next) {
 app.use(function(req, res) {
     res.status(404).send('La url solicitada no se encuentra.');
 });
+
+
+
+var socketCount = 0
+
+users = {}, socks = {};
+
+io.sockets.on("connection", function(socket) {
+
+    socketCount++;
+    io.sockets.emit('users connected', socketCount);
+
+
+    //  console.log(socket.handshake.session.user);
+    //console.log(socket.handshake.session.admin);
+});
+
+
 
 var port = process.env.PORT || 3000;
 server.listen(port, () => {
